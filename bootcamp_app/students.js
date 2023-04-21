f7707d4b89f57ff066c620d93d00b9c1ee8a838e
SEAF39 <labber@DESKTOP-7J1C764>
2023-04-21 00:21:21 -0400

    add Querey for students

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -32,7 +32,7 @@ LIMIT 5;
 
  */
 
-pool.query(`
+/* pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
@@ -42,4 +42,78 @@ LIMIT 5;
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+}); */
+
+
+/* pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack)); */
+
+/* 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(`
+SELECT students.id, students.name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
+.then(res => {
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
+ */
+
+/* 
+const query = {
+  text: `
+    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+    FROM students
+    JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name LIKE $1
+    LIMIT $2;
+  `,
+  values: [`%${process.argv[2]}%`, process.argv[3] || 5]
+};
+
+pool.query(query)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
+ */
+
+
+  const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  }).catch(err => console.error('query error', err.stack));
